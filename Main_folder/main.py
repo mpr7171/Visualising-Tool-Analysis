826e8f8c98790296c7e9c409a85f6730b2cfa217
Yash Mathur <63182778+dev-yashmathur@users.noreply.github.com>
2023-06-01 12:09:32 +0530

    Last changes

--- Main_folder/main.py
+++ Main_folder/main.py
@@ -44,7 +44,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message')
 def handle_message(message):
-    print("Received message: " + message)
+    # print("Received message: " + message)
     if not "User Connected!" in message:
         indexOfColon = message.index(':')
         sent_user = message[:indexOfColon]
